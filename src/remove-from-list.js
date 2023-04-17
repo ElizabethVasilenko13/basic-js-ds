7c745eea290d83e3a9fc59ee6c2860d4d05923be
Yelyzaveta Vasylenko <liza.vasilenko13@gmail.com>
2023-04-17 21:34:59 +0200

    feat: add remove from list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,31 +1,43 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
-/**
- * Given a singly linked list of integers l and an integer k,
- * remove all elements from list l that have a value equal to k.
- *
- * @param {List} l
- * @param {Number} k
- * @return {List}
- *
- * @example
- * For l = [3, 1, 2, 3, 4, 5] and k = 3,
- * the output should be [1, 2, 4, 5]
- *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
- */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(head, k) {
+  let previousNode = null;
+
+  while (head && head.value === k) {
+    head = head.next;
+  }
+
+  let current = head;
+  while (current) {
+    console.log(current);
+    if (current.value === k) {
+      previousNode.next = current.next;
+    } else {
+      previousNode = current;
+    }
+    current = current.next;
+  }
+  return head;
 }
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+const initial = convertArrayToList([3, 1, 2, 3, 4, 5]);
+    
+    
+console.log((removeKFromList(initial, 3)));
 
 module.exports = {
   removeKFromList
